d667bc004357bffb8fce4affb8e4db7c14c04a42
abelsouzacosta <abelsouzacosta@gmail.com>
2023-10-14 22:25:35 -0300

    [chore]: class Contract finished

--- src/domain/entities/Contract.java
+++ src/domain/entities/Contract.java
@@ -1,4 +1,31 @@
 package domain.entities;
 
+import java.util.Date;
+
 public class Contract {
+    private final Date date;
+    private final double valuePerHour;
+    private final int hours;
+
+    private Contract(Date date, double valuePerHour, int hours) {
+        this.date = date;
+        this.valuePerHour = valuePerHour;
+        this.hours = hours;
+    }
+
+    protected static Contract createContract(Date date, double valuePerHour, int hours) {
+        return new Contract(date, valuePerHour, hours);
+    }
+
+    protected Date getDate() {
+        return this.date;
+    }
+
+    protected double getValuePerHour() {
+        return this.valuePerHour;
+    }
+
+    protected int getHours() {
+        return this.hours;
+    }
 }

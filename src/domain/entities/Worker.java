00caa5db1e1b399e147e6da643b51e76d5b9f7ff
abelsouzacosta <abelsouzacosta@gmail.com>
2023-10-14 22:39:50 -0300

    [chore]: class Worker finished

--- src/domain/entities/Worker.java
+++ src/domain/entities/Worker.java
@@ -1,4 +1,31 @@
 package domain.entities;
 
+import domain.enums.WorkerLevel;
+
 public class Worker {
+    private final String name;
+    private final WorkerLevel level;
+    private final double baseSalary;
+
+    private Worker(String name, WorkerLevel level, double baseSalary) {
+        this.name = name;
+        this.level = level;
+        this.baseSalary = baseSalary;
+    }
+
+    protected static Worker createWorker(String name, WorkerLevel level, double baseSalary) {
+        return new Worker(name, level, baseSalary);
+    }
+
+    protected String getName() {
+        return this.name;
+    }
+
+    protected WorkerLevel getLevel() {
+        return this.level;
+    }
+
+    protected double getBaseSalary() {
+        return this.baseSalary;
+    }
 }
